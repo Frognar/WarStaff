5ac3b13d5788e1c26baefc07948f1f70f3dc9135
Frognar <sebastian.przyszlak@gmail.com>
2021-11-20 14:49:23 +0100

    Replase player health with health system

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,21 @@
+using UnityEngine;
+
 namespace Frognar {
-  public class Player : Movement {
-    Health health;
+  public class Player : Movement, Damageable {
+    HealthSystem healthSystem;
+    [SerializeField] IntVariable maxHealth;
+    HealthBar healthBar;
+
+    public void TakeDamage(int amount) {
+      healthSystem.TakeDamage(amount);
+    }
 
     protected override void Awake() {
       base.Awake();
-      health = GetComponent<Health>();
-      health.OnDeath += (s, e) => Destroy(gameObject);
+      healthSystem = new HealthSystem(maxHealth.Value);
+      healthBar = GetComponentInChildren<HealthBar>();
+      healthBar.SetHealthSystem(healthSystem);
+      healthSystem.OnDie += (s, e) => Destroy(gameObject);
     }
   }
 }
