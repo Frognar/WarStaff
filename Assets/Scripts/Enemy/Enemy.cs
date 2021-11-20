88b985a733bb74dade0ec5408ea797409cb63aac
Frognar <sebastian.przyszlak@gmail.com>
2021-11-20 14:57:18 +0100

    Replace enemy health with health system

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,20 +1,30 @@
+using UnityEngine;
+
 namespace Frognar {
-  public class Enemy : Movement {
-    Health health;
+  public class Enemy : Movement, Damageable {
+    HealthSystem healthSystem;
+    [SerializeField] IntVariable maxHealth;
+    HealthBar healthBar;
     Pool<Enemy> pool;
+
+    public void TakeDamage(int amount) {
+      healthSystem.TakeDamage(amount);
+    }
+
+    public void Reset() {
+      healthSystem.Reset();
+    }
 
     protected override void Awake() {
       base.Awake();
-      health = GetComponent<Health>();
-      health.OnDeath += (s, e) => pool.Release(this);
+      healthSystem = new HealthSystem(maxHealth.Value);
+      healthBar = GetComponentInChildren<HealthBar>();
+      healthBar.SetHealthSystem(healthSystem);
+      healthSystem.OnDie += (s, e) => pool.Release(this);
     }
 
     public void SetPool(Pool<Enemy> pool) {
       this.pool = pool;
-    }
-
-    public void Reset() {
-      health.Reset();
     }
   }
 }
